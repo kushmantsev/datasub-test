5292150b72cde0a9a5216c942135cb340e0658d3
Aleksandr Kushmantsev <a-kushmantsev@yandex.ru>
2021-12-04 22:58:52 +0400

    Fixed some sql

--- task2.sql
+++ task2.sql
@@ -1,35 +1,35 @@
 CREATE TABLE IF NOT EXISTS `Students` (
-	  `StudentId` INT(6) NOT NULL AUTO_INCREMENT,
-  	`FirstName` VARCHAR(256) NOT NULL,
-    `LastName` VARCHAR(256) NOT NULL,
-  	`Group` VARCHAR(256) NOT NULL,
-    PRIMARY KEY(`StudentId`)
+	`StudentId` INT(6) NOT NULL AUTO_INCREMENT,
+	`FirstName` VARCHAR(256) NOT NULL,
+	`LastName` VARCHAR(256) NOT NULL,
+	`Group` VARCHAR(256) NOT NULL,
+	PRIMARY KEY(`StudentId`)
 );
 
 CREATE TABLE IF NOT EXISTS `Exams` (
-	  `StudentId` INT(6) NOT NULL,
-  	`ExamName` VARCHAR(256) NOT NULL,
-  	`Result` INT(1) NOT NULL
+	`StudentId` INT(6) NOT NULL,
+	`ExamName` VARCHAR(256) NOT NULL,
+	`Result` INT(1) NOT NULL
 );
 
  /*Запрос про оценки*/
  
- SELECT DISTINCT `FirstName`, `LastName`, `Group` FROM `Students` s JOIN(
-  	SELECT `StudentId`, `ExamName`, `Result` FROM `Exams` WHERE `Result` < 3
+SELECT DISTINCT `FirstName`, `LastName`, `Group` FROM `Students` s JOIN(
+	SELECT `StudentId`, `ExamName`, `Result` FROM `Exams` WHERE `Result` < 3
   ) as e ON
-  s.StudentId = e.StudentId
-  GROUP BY e.StudentId
-  HAVING COUNT(*) > 2;
+	s.StudentId = e.StudentId
+	GROUP BY e.StudentId
+	HAVING COUNT(*) > 2;
   
 /*Запрос про группы*/  
 
- CREATE OR REPLACE VIEW `Groups` AS
- 	 SELECT `Group` FROM `Students` s JOIN(
-        SELECT `StudentId`, `Result` FROM `Exams` WHERE `Result` < 3
+CREATE OR REPLACE VIEW `Groups` AS
+	SELECT `Group` FROM `Students` s JOIN(
+		SELECT `StudentId`, `Result` FROM `Exams` WHERE `Result` < 3
   ) as e ON
-  s.StudentId = e.StudentId
-  GROUP BY e.StudentId
-  HAVING COUNT(*) > 2;
+	s.StudentId = e.StudentId
+	GROUP BY e.StudentId
+	HAVING COUNT(*) > 2;
   
-  SELECT DISTINCT `Group` FROM `Groups`
-  GROUP BY `Group` HAVING COUNT(*) > 10;
+SELECT DISTINCT `Group` FROM `Groups`
+GROUP BY `Group` HAVING COUNT(*) > 10;
